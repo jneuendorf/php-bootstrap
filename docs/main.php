cdf7774746599c28e99470f4dc40f16d0311abf1
Jim Neuendorf <jim.neuendorf@gmx.de>
2017-02-17 16:10:19 +0100

    including libs from cdn (in order to make docs work)

--- docs/main.php
+++ docs/main.php
@@ -89,9 +89,9 @@ function modal_doc_no_kwargs() {
 <head>
     <meta charset="utf-8">
     <title>php-bootstrap</title>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <script type="text/javascript" src="../js/jquery-2.2.2.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <style media="screen">
         body {
             background-color: rgb(236, 236, 236);
